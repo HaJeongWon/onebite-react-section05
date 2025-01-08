66575aa95f0cb9a1223528ea11cbccaf842819fa
JeongWon Ha <gkwjddnjs06@gmail.com>
2025-01-08 17:50:07 +0900

    Add useRef

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
@@ -12,19 +13,31 @@ const Register = () => {
     country: "",
     bio: "",
   });
+  const countRef = useRef(0);
+  const inputRef = useRef();
 
   const onChange = (e) => {
-    console.log(e.target.name, e.target.value);
+    // countRef.current++;
+    count++;
+    console.log(count);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소의 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -53,6 +66,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
